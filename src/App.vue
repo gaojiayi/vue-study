92ca2a367663d46575d9d436099fd1be38aed1c2
Gao <Jiayi.Gao@emc.com>
2019-10-22 16:28:40 +0800

    init code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" />
+    <router-view />
+    <hello></hello>
   </div>
 </template>
 
 <script>
+// 导入组件
+import Hello from "./components/Hello";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Hello
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
